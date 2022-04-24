b7864a694656086b335aaef29bb035b529013a47
nero <wnsgur765z@naver.com>
2022-04-24 18:40:12 +0900

    feat: Add router

---
+++ 11-express/9-cors/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(
+  cors({
+    origin: ['http://127.0.0.1:5500'],
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.listen(8080);
